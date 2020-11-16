5f3eea7bb5177749ba11e83ce1194701347f6cf7
ramcalm <ram.callm@gmail.com>
2020-11-16 16:16:11 +0530

    Displayed Character Details

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
 import React, { useState, useEffect} from 'react'
-import './App.css'
+import axios from 'axios'
 import Header from './components/UI/Header'
-
+import CharacterGrid from './components/Characters/CharacterGrid'
+import './App.css'
 const App = () => {
 
   const [items, setItems] = useState([])
@@ -14,14 +14,16 @@ const App = () => {
         'https://www.breakingbadapi.com/api/characters'
       )
       console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
     }
     fetchItems()
-
   }, [])
 
   return (
     <div className="container">
-      <Header />    
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />    
     </div>
   );
 }
