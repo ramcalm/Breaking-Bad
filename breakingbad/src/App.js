d54c0bd8b6dcb80cc33c6bcd527900c03ccd53a0
ramcalm <ram.callm@gmail.com>
2020-11-16 16:29:44 +0530

    Updated Cards

--- breakingbad/src/App.js
+++ breakingbad/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 import Header from './components/UI/Header'
+import Search from './components/UI/Search'
 import CharacterGrid from './components/Characters/CharacterGrid'
 import './App.css'
 const App = () => {
@@ -23,6 +24,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <Search />
       <CharacterGrid isLoading={isLoading} items={items} />    
     </div>
   );
